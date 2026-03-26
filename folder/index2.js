e3d59d1d2b8f0440d163143273962eea4c318660
Azarudeen <azarudeen.b2003@gmail.com>
2026-03-26 18:52:30 +0530

    node.js day 1

---
+++ folder/index2.js
@@ -0,0 +1,6 @@
+let a=[12,{name:"ilife",age:12},{name:"epix",age:12}]
+// console.log("Index 2 file",a,a[1].age)
+console.log(a.find((v,i)=>v == 12));
+
+// npm i nodemon => for live running
+// run command nodemon file_name
---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("Node js day 1")
+console.log("Node js day 2")
+console.log("Node js day 3")
+console.log("Node js day 4")
